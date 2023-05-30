61b0702c19e0533ca8eaaf91155d026460528a95
Hila Aslam <hilaaslam157@gmail.com>
2023-05-30 15:51:34 +0500

    ass-1 Q(6-7)

---
+++ ha/Assignments/Assignment-1/app.js
@@ -0,0 +1,17 @@
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// function showAlert() {
+    
+//     alert("hello");
+// }
+
+                    // // ANSWER IN index.html
+
+// // 7. Practice placement of <script></script> element in
+// // following sections of your project in exercise 6:
+// // a. Head
+// // b. Body (before your page’s HTML)
+// // c. Body (inside your page’s HTML)
+// // d. Body (after your page’s HTML)
+
+                    // // ANSWER IN index.html
